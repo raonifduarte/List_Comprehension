e1c85fa7a683a0b70d888ad3697db99fb28e5e5c
raonifduarte <80927549+raonifduarte@users.noreply.github.com>
2021-06-16 13:00:07 -0300

    Add files via upload

---
+++ comprehension_v1.py
@@ -0,0 +1,10 @@
+# [expressao, for, item in list, if condicional]
+dobros = [i * 2 for i in range(10)]
+print(dobros)
+
+# Versão "normal"
+
+dobros = []
+for i in range(10):
+    dobros.append(i * 2)
+print(dobros)
